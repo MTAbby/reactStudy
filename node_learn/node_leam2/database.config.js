422cfb5f0dd56a42ee40fe2e13ef199c1ec88155
shuman <1105850944@qq.com>
2018-12-03 17:53:12 +0800

    mysql封装

---
+++ node_learn/node_leam2/database.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: shuman
+ * @Date: 2018-12-03 15:25:34
+ * @LastEditors: shuman
+ * @LastEditTime: 2018-12-03 15:25:47
+ * @Description: 数据库配置
+ */
+
+module.exports = {
+  host     : 'localhost',
+  port : 3306,
+  user     : 'root',
+  password : 'shuman',
+  database : 'shuman_lx'
+}
--- node_learn/node_leam2/text.js
+++ node_learn/node_leam2/text.js
@@ -1,18 +1,35 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'shuman',
-  database : 'shuman_lx'
-});
- 
-connection.connect();
+let db = require('./db')
 
 // 查询
-connection.query('SELECT * from student', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
+db.query('SELECT * from student', [], (results, fields) => {
+  console.log('>>>>>>>>>>>', results)
 })
+
+// 新增
+let name = ['二货','哈哈','王小五','晓晓']
+for(let i = 2; i < 6; i++) {
+  let  userAddSql = 'INSERT INTO student(id,name) VALUES(?,?)'
+  let  userAddSql_Params = [i, name[i - 2]]
+  db.query(userAddSql, userAddSql_Params, (results, fields) => {
+    console.log('>>>>>>>>>>>', results)
+  })
+}
+
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'shuman',
+//   database : 'shuman_lx'
+// });
+ 
+// connection.connect();
+
+// 查询
+// connection.query('SELECT * from student', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results);
+// })
 
 // 新增
 // var  userAddSql = 'INSERT INTO student(id,name) VALUES(?,?)'
@@ -41,13 +58,13 @@ connection.query('SELECT * from student', function (error, results, fields) {
 // })
 
 // 删除
-var userDelSql = 'DELETE FROM student WHERE id = 2'
-connection.query(userDelSql, function(error, results){
-  if(error){
-    console.log('[DELETE ERROR] - ',error.message);
-    return
-  }       
-  console.log('-------------DELETE--------------');
-  console.log('DELETE affectedRows',results.affectedRows);
-  console.log('&&&&&&&&&&&&&&&&&')
-})
+// var userDelSql = 'DELETE FROM student WHERE id = 2'
+// connection.query(userDelSql, function(error, results){
+//   if(error){
+//     console.log('[DELETE ERROR] - ',error.message);
+//     return
+//   }       
+//   console.log('-------------DELETE--------------');
+//   console.log('DELETE affectedRows',results.affectedRows);
+//   console.log('&&&&&&&&&&&&&&&&&')
+// })
